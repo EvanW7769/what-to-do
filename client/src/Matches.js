c690f8400a5ef146d9ea1e2639c6ec747c05fa1a
Evan Watson <thomasevanwatson@gmail.com>
2022-05-11 15:22:46 -0400

    Second

---
+++ client/src/Matches.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Matches (){
+
+    return(
+        <div>
+            <h1>
+                Hello!
+            </h1>
+        </div>
+    )
+}
+
+export default Matches
